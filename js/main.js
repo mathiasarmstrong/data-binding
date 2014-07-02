fe35c3248bea95e11d5f28918a9d0832d63ab4a8
Shehzan Devani <shehzandevani@gmail.com>
2014-07-02 09:51:56 -0700

    Added binding in 1 direction

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
+$(document).ready(function() {
+  if (!window.nameTemplates) {
+    window.nameTemplates = [];
+  }
+  var elements = $('.use-name');
+
+  for (var i = 0; i < elements.length; i++) {
+    window.nameTemplates.push( [elements[i], elements[i].innerHTML] );
+  }
+});
+
 $('.model-name').on('change', function() {
   var name = this.value;
-  $('.use-name').each(function(index, elem){
-    elem = $(elem);
-    var template = elem.html();
+  $(window.nameTemplates).each(function(index, binding){
+    elem = $(binding[0]);
+    var template = binding[1]
     var newHtml = applyTemplate(template, {name: name})
     elem.html(newHtml);
   });
 });
+
 var applyTemplate = function(template, values){
   for (key in values) {
     var regex = new RegExp("{{" + key + "}}", "g");
     template = template.replace(regex, values[key])
   }
   return template;
-}
+};
